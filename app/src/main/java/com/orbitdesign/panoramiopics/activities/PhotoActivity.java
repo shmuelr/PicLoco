67b56007497a04b859c8dbcb837120a68d72b6d0
Shmuel Rosansky <sdrosansky@gmail.com>
2015-04-29 10:42:32 -0400

    Version 1.1
    Improve setting wallpaper
    Update MaterialEditText
    Replace ImageView in PhotoActivity with https://github.com/davemorrissey/subsampling-scale-image-view
    Update Android Support Libraries

--- app/src/main/java/com/orbitdesign/panoramiopics/activities/PhotoActivity.java
+++ app/src/main/java/com/orbitdesign/panoramiopics/activities/PhotoActivity.java
@@ -1,14 +1,11 @@
 package com.orbitdesign.panoramiopics.activities;
 
-import android.support.v7.app.AlertDialog;
-import android.app.WallpaperManager;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
@@ -28,25 +25,26 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.davemorrissey.labs.subscaleview.ImageSource;
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 import com.orbitdesign.panoramiopics.R;
 import com.orbitdesign.panoramiopics.models.Photo;
-import com.squareup.picasso.Callback;
-import com.squareup.picasso.Picasso;
-import com.squareup.picasso.Transformation;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Locale;
 
 public class PhotoActivity extends AppCompatActivity {
 
     private static final String TAG = "PhotoActivity";
-    private static final String APP_DIRECTORY = "/PanoramioPics";
+    private static final String APP_DIRECTORY = "/PicLoco";
 
     private Photo mPhoto;
 
@@ -56,7 +54,7 @@ public class PhotoActivity extends AppCompatActivity {
 
     private Bitmap largeBitmap = null;
 
-    private ImageView mPhotoImageView;
+    private SubsamplingScaleImageView mPhotoImageView;
     private TextView mTextViewDescriptionLine1, mTextViewDescriptionLine2, mTextViewDescriptionLine3, mTextViewDescriptionLine4;
     private CardView mCardView;
 
@@ -74,72 +72,18 @@ public class PhotoActivity extends AppCompatActivity {
 
         initGui();
 
+        // The small bitmap should be set by the main activity before launching this activity
         if(IMAGE_BITMAP_SMALL != null){
-            Palette palette = Palette.generate(IMAGE_BITMAP_SMALL.getBitmap());
+            Palette palette = Palette.from(IMAGE_BITMAP_SMALL.getBitmap()).generate();
             setupScreenUsingPalette(palette);
         }
 
-        // Use this to determine the maximum image size supported by the GPU
-        final Canvas canvas = new Canvas();
-
-        Callback picassoCallback = new Callback() {
-              @Override
-              public void onSuccess() {
-                  findViewById(R.id.progressBar).setVisibility(View.GONE);
-              }
-
-              @Override
-              public void onError() {
-                  findViewById(R.id.progressBar).setVisibility(View.GONE);
-              }
-        };
-
-        if(mPhoto.getWidth() > canvas.getMaximumBitmapWidth() || mPhoto.getHeight() > canvas.getMaximumBitmapHeight()){
-            Picasso.with(this)
-                    .load(mPhoto.getLargePhotoFileUrl())
-                    .transform(new Transformation() {
-                        @Override
-                        public Bitmap transform(Bitmap source) {
-                            largeBitmap = source;
-                            return source;
-                        }
-
-                        @Override
-                        public String key() {
-                            return "key";
-                        }
-                    })
-                    .resize(canvas.getMaximumBitmapWidth() - 500, canvas.getMaximumBitmapHeight() - 500)
-                    .centerInside()
-                    .placeholder(IMAGE_BITMAP_SMALL != null ? IMAGE_BITMAP_SMALL : new ColorDrawable(Color.DKGRAY))
-                    .into(mPhotoImageView, picassoCallback);
-        }else{
-            Picasso.with(this)
-                    .load(mPhoto.getLargePhotoFileUrl())
-                    .transform(new Transformation() {
-                        @Override
-                        public Bitmap transform(Bitmap source) {
-                            largeBitmap = source;
-                            return source;
-                        }
-
-                        @Override
-                        public String key() {
-                            return "key";
-                        }
-                    })
-                    .placeholder(IMAGE_BITMAP_SMALL != null ? IMAGE_BITMAP_SMALL : new ColorDrawable(Color.DKGRAY))
-                    .into(mPhotoImageView,picassoCallback );
-        }
-
-
-
-
+        new AsycBitmapFromURL().execute(mPhoto.getLargePhotoFileUrl());
 
     }
 
     private void initGui() {
-        mPhotoImageView = (ImageView) findViewById(R.id.photoImageView);
+        mPhotoImageView = (SubsamplingScaleImageView) findViewById(R.id.photoImageView);
 
         mCardView = (CardView)findViewById(R.id.metaDataCard);
         mTextViewDescriptionLine1 = (TextView)findViewById(R.id.textViewLine1);
@@ -231,24 +175,7 @@ public class PhotoActivity extends AppCompatActivity {
 
             return true;
         }else if (id == R.id.action_set_as_wallpaper) {
-
-            new AlertDialog.Builder(this)
-                    .setTitle("Set as wallpaper")
-                    .setMessage("Are you sure you want to set this image as your wallpaper?")
-                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            setPhotoAsWallpaper();
-                        }
-                    })
-                    .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-
-                        }
-                    })
-                    .show();
-
-
-
+            promptAndSetPhotoAsWallpaper();
             return true;
         }else if (id == R.id.action_save_to_phone) {
             Toast.makeText(this, "Photo saved", Toast.LENGTH_SHORT).show();
@@ -263,8 +190,7 @@ public class PhotoActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void sharePhoto() {
-
+    private Uri getPhotoUri(){
         File path = new File(Environment.getExternalStorageDirectory().toString()+ APP_DIRECTORY);
         File file = new File(path, mPhoto.getPhotoTitle()+".jpg");
         Uri photoLocation;
@@ -273,14 +199,15 @@ public class PhotoActivity extends AppCompatActivity {
         }else {
             photoLocation = savePhoto();
         }
+        return photoLocation;
+    }
 
-
-
+    private void sharePhoto() {
 
         final Intent shareIntent = new Intent(     android.content.Intent.ACTION_SEND);
         shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        shareIntent.putExtra(Intent.EXTRA_STREAM, photoLocation);
-        shareIntent.putExtra(Intent.EXTRA_TEXT, "sent from PanoramioPics app");
+        shareIntent.putExtra(Intent.EXTRA_STREAM, getPhotoUri());
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "sent from PicLoco app");
         shareIntent.setType("image/jpg");
         startActivity(shareIntent);
     }
@@ -320,7 +247,7 @@ public class PhotoActivity extends AppCompatActivity {
         }
 
         if(file.exists()){
-            Uri uri = addImageToGallery(this, file.toString(), mPhoto.getPhotoTitle(), "from PanoramioPic app");
+            Uri uri = addImageToGallery(this, file.toString(), mPhoto.getPhotoTitle(), "from PicLoco app");
 
             return uri;
         }else{
@@ -340,21 +267,67 @@ public class PhotoActivity extends AppCompatActivity {
         return context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
     }
 
-    private void setPhotoAsWallpaper() {
-        WallpaperManager wallpaperManager = WallpaperManager.getInstance(this);
-        try {
-            wallpaperManager.setBitmap(getPhotoBitmap());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private void promptAndSetPhotoAsWallpaper() {
 
+        Intent setAs = new Intent(Intent.ACTION_ATTACH_DATA);
+        setAs.setDataAndType(getPhotoUri(),"image/jpg");
+        setAs.putExtra("mimeType", "image/jpg");
+        startActivity(Intent.createChooser(setAs, "Set Image As"));
     }
 
     private Bitmap getPhotoBitmap(){
-        if(largeBitmap != null){
-            return largeBitmap;
+        return largeBitmap;
+    }
+
+    public void setLoading(Boolean isloading){
+        if(isloading) {
+            findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
         }else{
-            return ((BitmapDrawable)mPhotoImageView.getDrawable()).getBitmap();
+            findViewById(R.id.progressBar).setVisibility(View.GONE);
+        }
+    }
+
+    private class AsycBitmapFromURL extends AsyncTask<String, Void, Bitmap>{
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            setLoading(true);
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            Request request = new Request.Builder().url(params[0]).build();
+            Bitmap bitmap = null;
+            try {
+                InputStream is = okHttpClient.newCall(request).execute().body().byteStream();
+
+                bitmap = BitmapFactory.decodeStream(is);
+
+            } catch (IOException e) {
+                Log.e(TAG, e.getMessage());
+                e.printStackTrace();
+            }
+
+
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+
+            if (bitmap != null) {
+                largeBitmap = bitmap;
+                mPhotoImageView.setImage(ImageSource.bitmap(bitmap));
+            }else{
+                Log.e(TAG, "Bitmap is null");
+                Toast.makeText(PhotoActivity.this, "Oops, there was a mistake. Please contact me so I can fix it. Thanks!", Toast.LENGTH_LONG).show();
+            }
+
+            setLoading(false);
         }
     }
 
